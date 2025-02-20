f176f67d7c2ab1f1f6f971ff0a9b8cbecdfdf58c
Vlad <vladosmen29@gmail.com>
2025-02-20 13:37:58 +0600

    aded delete reminder and list reminder

--- whatsapp_reminder/src/reminders/api/urls.py
+++ whatsapp_reminder/src/reminders/api/urls.py
@@ -1,7 +1,9 @@
 from django.urls import path
-from .views import CreateReminderView
+from .views import CreateReminderView , DeleteReminderView , ListRemindersView
 
 urlpatterns = [
     path('create-reminder/', CreateReminderView.as_view(), name='create-reminder'),
+    path('delte-reminder/<int:reminder_id>/', DeleteReminderView.as_view(), name='delete-reminder'),
+    path('list-reminder/<str:phone_number>/', ListRemindersView.as_view(), name='list-reminder'),
 
 ]
--- whatsapp_reminder/src/reminders/api/views.py
+++ whatsapp_reminder/src/reminders/api/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework import status
+from reminders.api.serializers import ReminderSerializer
 from users.models import User
 from reminders.models import Reminder
 from reminders.services.whatsapp_service import GreenAPIService
@@ -28,13 +29,34 @@ class CreateReminderView(APIView):
         green_api.send_message(phone_number, confirmation_message)
 
         return Response({"message": "Reminder created successfully"}, status=status.HTTP_201_CREATED)
+    
+
+
+class ListRemindersView(APIView):
+    def get(self, request, phone_number):
+        user = User.objects.filter(phone_number=phone_number).first()
+        if not user:
+            return Response({"error": "User not found"}, status=status.HTTP_404_NOT_FOUND)
+        reminders = Reminder.objects.filter(user=user)
+        serializer = ReminderSerializer(reminders, many=True)
+        return Response(serializer.data)
 
 
 
 
 
 
-
+class DeleteReminderView(APIView):
+    """
+    Метод для удаления напоминания
+    """
+    def delete(self, request, reminder_id):
+        reminder = Reminder.objects.filter(id=reminder_id).first()
+        if not reminder:
+            return Response({"message": "Reminder not found"}, status=status.HTTP_404_NOT_FOUND)
+        
+        reminder.delete()
+        return Response({"message": "Reminder deleted successfully"}, status=status.HTTP_204_NO_CONTENT)
 
 
 
